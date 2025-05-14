aa969a4e2663a84c1bd4263be2eaeeaadc8f91e0
siddharth-reddy-1607 <sidd.reddy1607@gmail.com>
2025-05-14 10:33:52 +0530

    Add tests and break CI

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T){
+    headers := make(http.Header)
+    headers.Add("Accept","text/html")
+    headers.Add("Content","en-us")
+    apiKey,err := GetAPIKey(headers)
+    if err != ErrNoAuthHeaderIncluded && apiKey != ""{
+        t.Fatalf("With no authorization header: Expected err: %v and empty API Key but got err:%v and API Key:%v",ErrNoAuthHeaderIncluded,err,apiKey)
+    }
+    headers.Set("Authorization","ApiKey:myAPIKey")
+    apiKey,err = GetAPIKey(headers)
+    if err.Error() != "malformed authorization header" && apiKey != ""{
+        t.Fatalf("With malformed authorization header: Expected err: malformed authorization header and empty API Key but got err:%v and API Key:%v",err,apiKey)
+    }
+    headers.Set("Authorization","ApiKey myAPIKey")
+    apiKey,err = GetAPIKey(headers)
+    if err != nil && apiKey == ""{
+        t.Fatalf("With correct authorization header: Expected err: nil and non-empty API Key but got err:%v and API Key:%v",err,apiKey)
+    }
+}
+
